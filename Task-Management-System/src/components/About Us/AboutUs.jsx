db6cc031d55b1bfcf310fb977adb127adafb6b6e
AliveTube <92478946+AliveTube@users.noreply.github.com>
2024-10-06 23:47:51 +0300

    Added components folder

---
+++ Task-Management-System/src/components/About Us/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <>
+        <h1>About Us</h1>
+        <img src="" alt="About Task Management Website" />
+    </>
+  )
+}
+
+export default AboutUs
