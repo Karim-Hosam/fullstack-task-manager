1de64febc7fcb5a78fedbf1c0f117baa309d79d8
abwael <11410120210490@stud.cu.edu.eg>
2024-10-08 21:51:58 +0300

    Landing third section

--- Task-Management-System/src/components/Landing/Landing.jsx
+++ Task-Management-System/src/components/Landing/Landing.jsx
@@ -2,6 +2,7 @@ import NavBar from '../NavBar/NavBar.jsx';
 import FirstSection from './FirstSection';
 import SecondSection from './SecondSection';
 import LandingCSS from './Landing.module.css'
+import img2 from '../../Images/Landing-2.svg'
 
 export default function Landing(){
 
@@ -10,6 +11,14 @@ export default function Landing(){
         <>
             <FirstSection></FirstSection>
             <SecondSection></SecondSection>
+            <div className={LandingCSS.ThirdSection}>
+                <img src={img2} className={LandingCSS.img2} alt="Complete" />
+                <div className={LandingCSS.ThirdSecText}>
+                    <h2>About The Task Manager</h2>
+                    <p>The Task Management System is designed to help individuals and teams organize, prioritize, and manage their tasks more effectively. Built using modern technologies like React, Redux, and Node.js, this tool simplifies task tracking and boosts productivity through a streamlined, user-friendly interface.</p>
+                    <button>Learn More</button>                
+                </div>
+            </div>
         </>
     )
 }
