51254c1d2e7bee0def1e9594f0454089ca4e8f08
abwael <11410120210490@stud.cu.edu.eg>
2024-10-08 22:47:53 +0300

    Landing Forth Section

--- Task-Management-System/src/components/Landing/Landing.jsx
+++ Task-Management-System/src/components/Landing/Landing.jsx
@@ -1,8 +1,11 @@
-import NavBar from '../NavBar/NavBar.jsx';
 import FirstSection from './FirstSection';
 import SecondSection from './SecondSection';
+import ThirdSection from './ThirdSection';
+import ForthSection from './ForthSection';
 import LandingCSS from './Landing.module.css'
-import img2 from '../../Images/Landing-2.svg'
+
+
+
 
 export default function Landing(){
 
@@ -11,14 +14,8 @@ export default function Landing(){
         <>
             <FirstSection></FirstSection>
             <SecondSection></SecondSection>
-            <div className={LandingCSS.ThirdSection}>
-                <img src={img2} className={LandingCSS.img2} alt="Complete" />
-                <div className={LandingCSS.ThirdSecText}>
-                    <h2>About The Task Manager</h2>
-                    <p>The Task Management System is designed to help individuals and teams organize, prioritize, and manage their tasks more effectively. Built using modern technologies like React, Redux, and Node.js, this tool simplifies task tracking and boosts productivity through a streamlined, user-friendly interface.</p>
-                    <button>Learn More</button>                
-                </div>
-            </div>
+            <ThirdSection></ThirdSection>
+            <ForthSection></ForthSection>
         </>
     )
 }
