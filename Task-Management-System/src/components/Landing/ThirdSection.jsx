d007cac16962ba69370ec20b131e573cf23a478c
abwael <11410120210490@stud.cu.edu.eg>
2024-10-11 22:53:16 +0300

    Editing the landing

--- Task-Management-System/src/components/Landing/ThirdSection.jsx
+++ Task-Management-System/src/components/Landing/ThirdSection.jsx
@@ -1,8 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import LandingCSS from './Landing.module.css'
 import img2 from '../../Images/Landing-2.svg'
 import { Link } from 'react-router-dom'
 
 export default function ThirdSection(){
+
+    const navigate = useNavigate();
+
+    const navigateToAbout = () => {
+        navigate('/about');
+    }
 
     return(
         <>
@@ -11,7 +18,7 @@ export default function ThirdSection(){
                 <div className={LandingCSS.ThirdSecText}>
                     <h2>About The Task Manager</h2>
                     <p>The Task Management System is designed to help individuals and teams organize, prioritize, and manage their tasks more effectively. Built using modern technologies like React, Redux, and Node.js, this tool simplifies task tracking and boosts productivity through a streamlined, user-friendly interface.</p>
-                    <Link to = "/about" className={LandingCSS.LearnMore}>Learn More</Link>
+                    <button to = "/about" className={LandingCSS.LearnMore} onClick={navigateToAbout}>Learn More</button>
                                  
                 </div>
             </div>
