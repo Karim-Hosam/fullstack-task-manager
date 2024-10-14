90ea62b1cfbd5a91fe9400e705ea89405e5f921d
AliveTube <92478946+AliveTube@users.noreply.github.com>
2024-10-14 03:47:38 +0300

    Added aside bar functionality

--- Task-Management-System/Server/config/dbConnection.js
+++ Task-Management-System/Server/config/dbConnection.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
-const Connetion = mysql.createConnection({
+const Connection = mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'',
     database:'mysql2'
 })
 
-module.exports = Connetion;
+module.exports = Connection;
