73356fbe335bb078d076bef34cebac4ff4f6d978
abwael <11410120210490@stud.cu.edu.eg>
2024-10-17 02:29:50 +0300

    Profile Design V1.0

--- Task-Management-System/src/components/profile/ProfileInfo.jsx
+++ Task-Management-System/src/components/profile/ProfileInfo.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import Profile from './Profile.module.css';
+import ProfileCSS from './Profile.module.css';
 
 const ProfileInfo = ({ profile }) => {
     // const altText = profile.name ? profile.name.charAt(0) : 'P'; // First letter of the name or default 'P'
 
     return (
-        <div className={Profile['profile-info-container']}>
-            <h1 className={Profile['profile-header']}>{profile.username || 'Your Profile'}</h1>
-            <p className={Profile['profile-info']}>Email: {profile.email || 'No email available'}</p>
-            <p className={Profile['profile-info']}>Phone: {profile.phoneNumber || 'No phone number available'}</p>
+        <div className={ProfileCSS['profileInfoContainer']}>
+            <div className={ProfileCSS.cover}>
+                <div className={ProfileCSS.ProfilePic}>
+                        <h2>{profile.username.charAt(0)}</h2>
+                </div>
+            </div>
+            <div className={ProfileCSS.profileText}>
+                <h1 className={ProfileCSS['profile-header']}>{profile.username || 'Your Profile'}</h1>
+                <p className={ProfileCSS['profile-info']}>{profile.email || 'No email available'}</p>
+                <p className={ProfileCSS['profile-info']}>{profile.phoneNumber || 'No phone number available'}</p>
+            </div>
 
             {/* {profile.profilePicture ? (
                 <img
