680a8d3244c07ffedf662417b052ad3c899cadaa
abwael <11410120210490@stud.cu.edu.eg>
2024-10-17 00:41:12 +0300

    Update Tasks V1.0

--- Task-Management-System/src/components/UpdateTask/UpdateTask.jsx
+++ Task-Management-System/src/components/UpdateTask/UpdateTask.jsx
@@ -2,11 +2,13 @@ import axios from 'axios';
 import CreateUpdate from '../CreateUpdateTaskTemplate/CreateUpdate';
 import { useParams,useNavigate } from 'react-router-dom';
 import { useState,useEffect } from 'react';
+import { useOutletContext } from 'react-router-dom';
 
 export default function UpdateTask(){
     const [task, setTask] = useState({});
     const [loading, setLoading] = useState(true);
     const { uniqueId } = useParams();
+    const { updateTasks } = useOutletContext();
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -25,8 +27,24 @@ export default function UpdateTask(){
     }, [uniqueId]);
 
 
-    const handleSubmit = (task) => {
-        
+    //TO BE CHANGED
+    const navigateToTasks = () => {
+        navigate('/home/tasks');
+    };
+
+    const handleSubmit = async (task) => {
+        try {
+            await axios.put(`http://localhost:3000/api/tasks/${uniqueId}`, task);
+            alert('Task updated successfully');
+
+            const res = await axios.get('http://localhost:3000/api/tasks');
+            updateTasks(res.data);
+
+            navigate('/home/tasks');
+        } catch (error) {
+            console.error('Error updating task:', error);
+            alert('Failed to update task. Please try again.');
+        }
     };
 
     return(
