8ab5e90717759bbae58e4526d5475ed80ff8439f
abwael <11410120210490@stud.cu.edu.eg>
2024-10-17 22:25:46 +0300

    Landing Pictures

--- Task-Management-System/src/components/Landing/ForthSection.jsx
+++ Task-Management-System/src/components/Landing/ForthSection.jsx
@@ -1,6 +1,10 @@
 import LandingCSS from './Landing.module.css'
 import Card from './Card';
-import tempProfile from '../../Images/Temp-Profile.jpg'
+import tempProfile from '../../Images/TeamPics/NoPic.png'
+import Abdelrahman from '../../Images/TeamPics/Abdelrahman.png'
+import Omar from '../../Images/TeamPics/Omar.png'
+import Karim from '../../Images/TeamPics/Karim.png'
+import Abdelwahab from '../../Images/TeamPics/Abdelwahab.png'
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';  
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -13,10 +17,10 @@ export default function ForthSection(){
             <div className={LandingCSS.ForthSection}>
                 <h2>Our Team</h2>
                 <div className={LandingCSS.Cards}>
-                    <Card Name='Abdelrahman Wael' Image={tempProfile}></Card>
-                    <Card Name='Omar Mohamed' Image={tempProfile}></Card>
-                    <Card Name='Karim Hosam' Image={tempProfile}></Card>
-                    <Card Name='Ahmed Mohamed' Image={tempProfile}></Card>
+                    <Card Name='Abdelrahman Wael' Image={Abdelrahman}></Card>
+                    <Card Name='Omar Mohamed' Image={Omar}></Card>
+                    <Card Name='Karim Hosam' Image={Karim}></Card>
+                    <Card Name='Ahmed Mohamed' Image={Abdelwahab}></Card>
                     <Card Name='Mostafa Ahmed' Image={tempProfile}></Card>
                     <Card Name='Ziad Hassan' Image={tempProfile}></Card>
                 </div>
